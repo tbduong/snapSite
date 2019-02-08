ae03f0de42ee99db8da2802fab084e62bfb59cfe
tbduong <tiffaniduong@gmail.com>
2019-02-07 19:34:54 -0800

    add vector hero io image and containers

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+let express = require('express'),
+    path = require('path');
+var app = express();
+let server = require('http').Server(app);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(req, res, next){
+    res.sendStatus(200);
+});
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
