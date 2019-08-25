4205d9d27595b9654ca9998c6595ab84005f0037
tbduong <tiffaniduong@gmail.com>
2019-08-24 19:54:53 -0700

    add responsive images, update bg with animation css

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 $( document ).ready(function() {
 
+  //Change nav font color on bg color change
+  // $('.moduleGroup').scroll(function (e) {
+  //   e.preventDefault();
+  //     var pos = $('.moduleGroup').prop('scrollTop');
+  //     console.log(pos);
+  //
+  //     if  (pos >= 1900) {
+  //        $(".nav-right a, .navbar-brand").addClass('transition-nav');
+  //      } else {
+  //        $(".nav-right a, .navbar-brand").removeClass('transition-nav');
+  //      }
+  // });
 
-
-  $('.moduleGroup').scroll(function (e) {
-    e.preventDefault();
-      var pos = $('.moduleGroup').prop('scrollTop');
-      console.log(pos);
-
-      if (pos > 1184) {
-         $(".nav-right a, .navbar-brand").addClass('transition-nav');
-       } else {
-         $(".nav-right a, .navbar-brand").removeClass('transition-nav');
-       }
-
-
-  });
+  $('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    /* Just changed this to get the bottom dots navigation */
+    infinite: true,
+    speed: 500,
+    arrows: true
+    });
 
 
 
