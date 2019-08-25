a3abe6392a667c85b723a1f6a76969e1b61597a3
tbduong <tiffaniduong@gmail.com>
2019-08-25 15:53:10 -0700

    Change contact us section and WIP for portfolio work section

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 $( document ).ready(function() {
 
   //Change nav font color on bg color change
-  // $('.moduleGroup').scroll(function (e) {
-  //   e.preventDefault();
-  //     var pos = $('.moduleGroup').prop('scrollTop');
-  //     console.log(pos);
-  //
-  //     if  (pos >= 1900) {
-  //        $(".nav-right a, .navbar-brand").addClass('transition-nav');
-  //      } else {
-  //        $(".nav-right a, .navbar-brand").removeClass('transition-nav');
-  //      }
-  // });
+  $('.moduleGroup').scroll(function (e) {
+    e.preventDefault();
+      var pos = $('.moduleGroup').prop('scrollTop');
+      console.log(pos);
+
+      if  (pos >= 200) {
+         $('.scrolldown-arrow').hide();
+       } else {
+         $('.scrolldown-arrow').show();
+       }
+  });
 
   $('.slider').slick({
+    autoplay: true,
     centerMode: true,
     centerPadding: '60px',
-    /* Just changed this to get the bottom dots navigation */
     infinite: true,
-    speed: 500,
-    arrows: true
+    speed: 600,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    //override default text render for arrows
+    prevArrow: '<a class="slick-prev "></a>',
+		nextArrow: '<a class="slick-next "></a>'
     });
-
-
-
 
 
 });
